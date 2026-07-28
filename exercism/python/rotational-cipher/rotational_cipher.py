30b3b94675eb6897bac396516671eeed3af662a5
Aditya Alok <dev.aditya.alok@gmail.com>
2026-07-29 01:29:52 +0530

    exercism/python/rotational-cipher: faster implementation using native python functions
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- exercism/python/rotational-cipher/rotational_cipher.py
+++ exercism/python/rotational-cipher/rotational_cipher.py
@@ -1,4 +1,29 @@
-def rotate(text: str, key: int):
+def rotate(text: str, key: int) -> str:
+    """Rotates the given text as per the key"""
+
+    # How does it work?
+    # - find upto what index we should rotate the text
+    # - create translation and then apply that.
+    # - example: key = 2; i.e a -> c (1+2) and so on; thus
+    # we move 'ab' to last ...
+
+    key %= 26
+
+    alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    return text.translate(
+        str.maketrans(
+            alphabets,
+            alphabets[key:26]
+            + alphabets[:key]
+            + alphabets[key + 26 :]
+            + alphabets[26 : 26 + key],
+        )
+    )
+
+
+# Previous implementation.
+def rotate1(text: str, key: int):
     cipher_text = ""
 
     for char in text:
@@ -7,8 +32,6 @@ def rotate(text: str, key: int):
 
         position = ordinal - base
 
-        cipher_text += (
-            chr((position + key) % 26 + base) if 0 <= position <= 26 else char
-        )
+        cipher_text += chr((position + key) % 26 + base) if 0 <= position < 26 else char
 
     return cipher_text
